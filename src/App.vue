ad348e5cf59fad3237a70ba1e266bdb4cec751f1
Ramiz Raja <ramizpro@gmail.com>
2024-03-03 18:33:00 +0100

    Add router

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,11 @@ const env = computed(() => import.meta.env);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+  <h1>View</h1>
+  <router-link to="/">Home</router-link> |
+  <router-link to="/about">About</router-link>
+  <br />
+  <router-view/>
   <pre>{{ env }}</pre>
 </template>
 
